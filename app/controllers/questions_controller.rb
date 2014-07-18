b2f8522e95b747d7f239dd6a641721833dffd751
Raghu Betina <git@rugb.com>
2014-07-18 10:57:51 -0500

    Added solutions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,9 @@ class QuestionsController < ApplicationController
   def question_1
     # How many movies on the list did Francis Ford Coppola direct?
 
-    # @number_of_movies_by_coppola = ???
+    the_director = Director.find_by({ :name => "Francis Ford Coppola" })
+
+    @number_of_movies_by_coppola = the_director.movies.count
   end
 
   def question_2
@@ -10,7 +12,7 @@ class QuestionsController < ApplicationController
 
     # Your Ruby goes here.
 
-    # @director_of_oldest_movie = ???
+    @director_of_oldest_movie = Movie.order("year ASC").first.director
   end
 
   def question_3
@@ -18,7 +20,9 @@ class QuestionsController < ApplicationController
 
     # Your Ruby goes here.
 
-    # @oldest_movie_by_coppola = ???
+    the_director = Director.find_by({ :name => "Francis Ford Coppola" })
+
+    @oldest_movie_by_coppola = the_director.movies.order("year ASC").first
   end
 
   def question_4
@@ -26,14 +30,18 @@ class QuestionsController < ApplicationController
 
     # Your Ruby goes here.
 
-    # @most_recent_movie_by_coppola = ???
+    the_director = Director.find_by({ :name => "Francis Ford Coppola" })
+
+    @most_recent_movie_by_coppola = the_director.movies.order("year DESC").first
   end
 
   def question_5
     # What is the most recent movie on the list that Harrison Ford appeared in?
 
+    the_actor = Actor.find_by({ :name => "Harrison Ford" })
+
     # Your Ruby goes here.
 
-    # @most_recent_movie_by_ford = ???
+    @most_recent_movie_by_ford = the_actor.movies.order("year DESC").first
   end
 end
